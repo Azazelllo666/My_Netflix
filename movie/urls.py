e383d4d65271634c025f903e9eb6554321b1d892
Azazelllo666 <leonidovpi@yandex.ru>
2021-02-23 17:28:21 +0300

    Сделал вывод каждого фильма в отдельный шаблон

--- movie/urls.py
+++ movie/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from movie import views as movie
 
 urlpatterns = [
-    path('', movie.MovieView.as_view()),
+    path('', movie.MovieView.as_view(), name='movie_list'),
+    path('<slug:slug>/', movie.MovieDetailView.as_view(), name='movie_detail'),
 ]
