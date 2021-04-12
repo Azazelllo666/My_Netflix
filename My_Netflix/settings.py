c39f8c649f7cbb8d20a836292e762b05eb627683
azazello <leonidovpi@yandex.ru>
2021-04-12 20:27:02 +0300

    Обновил gitignore, настроил secret_key

--- My_Netflix/settings.py
+++ My_Netflix/settings.py
@@ -20,7 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-9ha5u2vra+9nxn+e-*&v5gnpa9nxl$)cu)mrie2i9qg!sq37&'
+try:
+    with open("secrets/secret_key.json", "r") as f:
+        SK = json.load(f)
+
+    SECRET_KEY = SK["SECRET_KEY_LOCAL"]
+except Exception as exp:
+    print("Settings loading fail: %s" % (exp))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
